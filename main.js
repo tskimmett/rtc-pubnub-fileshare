f0d7ef7cf08b6e77a271c10b4398707a5ef24306
Taylor Kimmett <taylor@pubnub.com>
2013-06-10 16:32:14 -0700

    Add Firefox Nightly support

--- main.js
+++ main.js
@@ -2,6 +2,19 @@
 var OFFER = "offer";
 var ANSWER = "answer";
 
+var IS_CHROME = !!window.webkitRTCPeerConnection;
+
+if (IS_CHROME) {
+	RTCPeerConnection = webkitRTCPeerConnection;
+	RTCIceCandidate = webkitRTCIceCandidate;
+	RTCSessionDescription = webkitRTCSessionDescription;
+}
+else {
+	RTCPeerConnection = mozRTCPeerConnection;
+	RTCIceCandidate = mozRTCIceCandidate;
+	RTCSessionDescription = mozRTCSessionDescription;
+}
+
 var pubnub = PUBNUB.init({
 	publish_key: 'pub-c-b2d901ee-2a0f-4d89-8cd3-63039aa6dd90',
 	subscribe_key: 'sub-c-c74c7cd8-cc8b-11e2-a2ac-02ee2ddab7fe'
@@ -12,18 +25,21 @@ function FSClient() {
 	this.uuid = pubnub.uuid();
 	this.fileName = null;
 	this.buffer = null;
-	this.chunkSize = 1000;
+	this.chunkSize = 800;
+	this.reqTimeoutThreshold = 500;
+	this.reqTimeout = null;
 	this.fileChunks = [];
 	this.nChunksReceived = 0;
 	this.nChunksExpected = 0;
 	this.localIceCandidates = [];
 	this.remoteIceCandidates = [];
-	var configuration = { 'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }] };
-	this.peerConn = new webkitRTCPeerConnection(configuration, {
+	var configuration = { 'iceServers': [{ 'url': (IS_CHROME ? 'stun:stun.l.google.com:19302' : 'stun:23.21.150.121') }] };
+	var opt = {
 		optional: [
 			{ RtpDataChannels: true }
 		]
-	});
+	};
+	this.peerConn = new RTCPeerConnection( configuration, (IS_CHROME ? opt : {}) );
 
 	// Create event callbacks
 	this.createPeerConnCallbacks();
@@ -47,7 +63,11 @@ FSClient.prototype = {
 			-MUST create channel before createOffer
 			-Chrome requires {reliable: false}
 		***/
-		this.dataChannel = this.peerConn.createDataChannel('RTCDataChannel', { reliable: false });
+		var dict = {};
+		if (window.webkitRTCPeerConnection) {
+			dict.reliable = false;
+		}
+		this.dataChannel = this.peerConn.createDataChannel('rtc-pubnub-fshare', dict);
 		this.registerChannelEvents();
 
 		this.peerConn.createOffer(this.onDescAvail.bind(this),
@@ -114,36 +134,35 @@ FSClient.prototype = {
 	createChannelCallbacks: function () {
 		var self = this;
 		this.onChannelMessage = function (msg) {
-			setTimeout(function (msg) {
-				console.log("vvv DataChannel msg vvv");
-				console.log(msg);
-				var data = JSON.parse(msg.data);
-				if (!isNaN(data.id)) {
+			//console.log("vvv DataChannel msg vvv");
+			//console.log(msg);
+			var data = JSON.parse(msg.data);
+			if (data.action === "DATA") {
+				clearTimeout(this.reqTimeout);
+				if (!self.fileChunks[data.id]) {
 					self.fileChunks[data.id] = Base64Binary.decode(data.content);
 					self.nChunksReceived++;
 					if (!self.checkDownloadComplete()) {
-						self.send(JSON.stringify({
-							ack: data.id + 1
-						}));
+						self.requestChunk(data.id + 1);
 					}
 					else {
 						console.log("Last chunk received.");
 						self.prepareFileDownload();
 					}
 				}
-				else if (!isNaN(data.ack)) {
-					console.log("Peer requesting chunk " + data.ack);
-					self.send(self.packageChunk(data.ack));
-				}
-			}.bind(self, msg), 200);
+			}
+			else if (data.action === "REQ") {
+				console.log("Peer requesting chunk " + data.id);
+				self.send(self.packageChunk(data.id));
+			}
 		};
 
 		this.onChannelReadyStateChange = function (e) {
 			console.log("Channel state: " + e.type);
 			if (e.type == "open") {
-				if (self.isInitiator) {
+				if (!self.isInitiator) {
 					// Ready to communicate data now
-					self.beginDataTransfer();
+					self.requestChunk(0);
 				}
 			}
 		};
@@ -157,6 +176,8 @@ FSClient.prototype = {
 	},
 
 	stageFileData: function (fName, fType, buffer) {
+		this.fileName = fName;
+		this.fileType = fType;
 		this.buffer = buffer;
 		var nChunks = Math.ceil(buffer.byteLength / this.chunkSize);
 		this.fileChunks = new Array(nChunks);
@@ -165,8 +186,6 @@ FSClient.prototype = {
 			start = i * this.chunkSize;
 			this.fileChunks[i] = buffer.slice(start, start + this.chunkSize);
 		}
-		this.fileName = fName;
-		this.fileType = fType;
 		document.querySelector("#shareFile").disabled = "";
 		console.log("File data staged");
 	},
@@ -183,24 +202,31 @@ FSClient.prototype = {
 		link.click();
 	},
 
-	beginDataTransfer: function () {
-		var self = this;
-		console.log("Begin data transfer...");
-		// Send each chunk over the channel
-		this.send(this.packageChunk(0));
-	},
-
 	send: function (data) {
 		this.dataChannel.send(data);
 	},
 
 	packageChunk: function (chunkId) {
 		return JSON.stringify({
+			action: "DATA",
 			id: chunkId,
 			content: Base64Binary.encode(this.fileChunks[chunkId])
 		});
 	},
 
+	requestChunk: function (chunkId) {
+		console.log("Request chunk " + chunkId);
+		var req = JSON.stringify({
+			action: "REQ",
+			id: chunkId
+		});
+		this.send(req);
+		//this.reqTimeout = setTimeout(this.send.bind(this, req), this.reqTimeoutThreshold);
+	},
+
+	/**
+		"Signals" are sent via PubNub until the DataChannel is established
+	**/
 	handleSignal: function (msg) {
 		var self = this;
 		//console.log(msg);
@@ -245,10 +271,13 @@ FSClient.prototype = {
 
 var client = new FSClient();
 
-document.onready = function () {
+console.log("Adding event listener");
+
+document.addEventListener("DOMContentLoaded", function () {
+	console.log("DOM Loaded");
 	var fInput = document.querySelector("#fileInput");
 	var file;
-	fInput.onchange = function (e) {
+	fInput.addEventListener("change", function (e) {
 		file = fInput.files[0];
 		if (file) {
 			var reader = new FileReader();
@@ -259,23 +288,23 @@ document.onready = function () {
 			};
 			reader.readAsArrayBuffer(file);
 		}
-	};
+	});
 
 	var gfButton = document.querySelector("#getFile");
-	gfButton.onclick = function (e) {
+	gfButton.addEventListener("click", function (e) {
 		// Once we're receiving data, we can't initiate anymore streaming
 		gfButton.disabled = "disabled";
 		fInput.disabled = "disabled";
 
 		client.answerShare();
-	};
+	}, false);
 
 	var sfButton = document.querySelector("#shareFile");
-	sfButton.onclick = function (e) {
+	sfButton.addEventListener("click", function (e) {
 		// Once we're sending data, we can't initiate anymore sharing
 		fInput.disabled = "disabled";
 		sfButton.disabled = "disabled";
 
 		client.offerShare();
-	};
-};
+	});
+});;
